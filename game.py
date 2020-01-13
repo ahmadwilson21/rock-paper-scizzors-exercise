8034ea1e15f183ac4ce2b2f21b47d41d64c5c552
ahmadwilson21 <50254137+ahmadwilson21@users.noreply.github.com>
2020-01-12 22:26:16 -0500

    Works need to modify losing code. Stuck in losing streak

--- game.py
+++ game.py
@@ -1,32 +1,51 @@
 import random
-choices = ["rock", "paper", "scizzors"]
 
-print("This game is called Rock, Paper, Scizzors, Shoot!")
-user_choice = (input("Enter Rock, Paper, or Scizzors")
-while user_choice.lower() != "rock" and user_choice.lower() != "paper" and user_choice.lower() != "scizzors":
-    print("Try again")
-    user_choice = (input("Enter Rock, Paper, or Scizzors")
-print("You chose " + user_choice)
+def PlayGame():
 
-computer_choice = random.choice(choices)
-print("Your opponent chose " + computer_choice)
-tie_flag = True
+    user_choice = (input("Enter Rock, Paper, or Scissors   "))
+    while (user_choice.lower() != "rock" and user_choice.lower() != "paper" and user_choice.lower() != "scissors") :
+        print("Try again")
+        user_choice = (input("Enter Rock, Paper, or Scissors   "))
+    print("You chose " + user_choice)
+    return user_choice.lower()
+    
 
-while(tie_flag == True):
 
-    if (computer_choice.lower() == user_choice):
-        print("Tie play again")
+def WholeGame(option, list):
+
+    computer_choice = random.choice(list)
+    print("Your opponent chose " + computer_choice + " ")
+    tie_flag = True
+    retry = False
+    while(tie_flag == True):
+
+        if retry == True:
+            retry == False
+            option == PlayGame()
+        if computer_choice == option:
+            print("Tie play again")
+            option = PlayGame()
         
-    elif (computer_choice.lower() == "rock" and user_choice.lower() == "paper") :
-        retry = input("Sorry you lost press any key to continue playing") 
-    elif (computer_choice.lower() == "paper" and user_choice.lower()== "scizzors"):
-        retry = input("Sorry you lost press any key to continue playing")
-    elif (computer_choice.lower() == "scizzors" and user_choice.lower()== "rock"):
-        retry = input("Sorry you lost press any key to continue playing")
-    else:
-        "You win!"
-        tie_flag = False
+        
+        elif (computer_choice == "scissors" and option == "paper") :
+            retry = input("Sorry you lost press any key to continue playing ")
+            print(retry)
+            retry = True
+        elif (computer_choice == "rock" and option== "scissors"):
+            retry = input("Sorry you lost press any key to continue playing ")
+            print(retry)
+            retry = True
+        elif (computer_choice == "paper" and option == "rock"):
+            retry = input("Sorry you lost press any key to continue playing ")
+            print(retry)
+            retry = True
+        else:
+            print("You win!")
+            tie_flag = False
     
 
             
-    
+choices = ["rock", "paper", "scissors"]
+print("This game is called Rock, Paper, Scissors, Shoot!")
+option = PlayGame()
+WholeGame(option, choices)
