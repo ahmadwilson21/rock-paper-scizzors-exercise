ba76c95e56d742d7ff9cac13ef6f50ab45f07fef
ahmadwilson21 <50254137+ahmadwilson21@users.noreply.github.com>
2020-01-29 10:46:11 -0500

    Deleted retry code and adjusted print statements for space (FIN)

--- game.py
+++ game.py
@@ -2,11 +2,11 @@ import random
 
 def PlayGame():
 
-    user_choice = (input("Enter Rock, Paper, or Scissors   "))
+    user_choice = (input("Please choose either 'rock', 'paper', or 'scissors':  "))
     while (user_choice.lower() != "rock" and user_choice.lower() != "paper" and user_choice.lower() != "scissors") :
         print("Try again")
-        user_choice = (input("Enter Rock, Paper, or Scissors   "))
-    print("You chose " + user_choice)
+        user_choice = (input("Please choose either 'rock', 'paper', or 'scissors':  "))
+    print("You chose: " + user_choice)
     return user_choice.lower()
     
 
@@ -14,45 +14,73 @@ def PlayGame():
 def WholeGame(option, lists):
 
     computer_choice = random.choice(lists)
-    print("Your opponent chose " + computer_choice + " ")
+    print("The computer chose " + computer_choice + " ")
+    print("-------------------")
     tie_flag = True
     retry = False
     while(tie_flag == True):
 
-        if retry == True:
-            #retry = False
-            newOption = PlayGame()
-            if (WholeGame(newOption, lists) == True):
-                quit()
 
+
+
+        ### This was originally supposed to be a case for if there was a retry for the game ###
+
+
+       #if retry == True:
+       #    #retry = False
+       #    newOption = PlayGame() #
+       #    if (WholeGame(newOption, lists) == True):
+       #        quit()
+
+        #else:
+        if computer_choice == option:
+            print("Tie play again")
+            tie_flag = False
+            #retry = True
+    
+    
+        elif (computer_choice == "scissors" and option == "paper") :
+            print("Oh, the computer won. It's ok.")
+            tie_flag = False
+            #print(retry)
+            #retry = True
+        elif (computer_choice == "rock" and option== "scissors"):
+            print("Oh, the computer won. It's ok." )
+            tie_flag = False
+            #print(retry)
+            #retry = True
+        elif (computer_choice == "paper" and option == "rock"):
+            print ("Oh, the computer won. It's ok." )
+            tie_flag = False
+            #print(retry)
+            #retry = True
         else:
-            if computer_choice == option:
-                print("Tie play again")
-                retry = True
+            print("You win!")
+            tie_flag = False
+            #return True
         
-        
-            elif (computer_choice == "scissors" and option == "paper") :
-                retry = input("Sorry you lost your opponent chose " + computer_choice+" press any key to continue playing " )
-                print(retry)
-                retry = True
-            elif (computer_choice == "rock" and option== "scissors"):
-                retry = input("Sorry you lost your opponent chose " + computer_choice+" press any key to continue playing " )
-                print(retry)
-                retry = True
-            elif (computer_choice == "paper" and option == "rock"):
-                retry = input("Sorry you lost your opponent chose " + computer_choice+" press any key to continue playing " )
-                
-                print(retry)
-                retry = True
-            else:
-                print("You win!")
-                tie_flag = False
-                return True
-        
+        print("-------------------")
+        print("Thanks for playing. Please play again!\n")
     
 
             
 choices = ["rock", "paper", "scissors"]
-print("This game is called Rock, Paper, Scissors, Shoot!")
+
+print("\n-------------------")
+print("Welcome to my Rock-Paper-Scissors game...")
+print("-------------------")
+
+#print("You chose: ",)
 option = PlayGame()
 WholeGame(option, choices)
+
+#-------------------
+#Welcome to my Rock-Paper-Scissors game...
+#-------------------
+#Please choose either 'rock', 'paper', or 'scissors': rock
+#You chose: 'rock'
+#The computer chose: 'paper'
+#-------------------
+#Oh, the computer won. It's ok.
+#-------------------
+#Thanks for playing. Please play again!
